044b71b9640ad0cc621a5973d37e8f2171b6a037
rghtd <4896526+rghtd@users.noreply.github.com>
2019-04-27 17:10:48 -0400

    Async working, need to fix query

--- pcapmap.py
+++ pcapmap.py
@@ -2,96 +2,159 @@
 
 from scapy.layers.inet import IP, TCP, UDP
 from scapy.sendrecv import sniff
-
-import argparse, bisect, socket
+from twisted.names import client
+from twisted.internet import defer, reactor, task
+import argparse, socket, time, threading, sys
 
 ip_src = ''
 ip_dest = ''
 tcp_sport = 0
 tcp_dport = 0
-
-
+num_packets_parsed = 0
+start = time.time()
 
 def parse_packet(packet):
-    if IP in packet:
+    global num_packets_parsed
+
+    if TCP in packet:
         host_list.add_host(packet[IP].src)
         host_list.add_host(packet[IP].dst)
-        if TCP in packet:
-            host_list.add_tcp_port(packet[IP].src, packet[TCP].sport)
-            host_list.add_tcp_port(packet[IP].dst, packet[TCP].dport)
-        if UDP in packet:
-            host_list.add_udp_port(packet[IP].src, packet[UDP].sport)
-            host_list.add_udp_port(packet[IP].dst, packet[UDP].dport)
+        host_list.add_tcp_port(packet[IP].src, packet[TCP].sport, Host.PORT_SRC)
+        host_list.add_tcp_port(packet[IP].dst, packet[TCP].dport, Host.PORT_DST)
+    elif UDP in packet:
+        host_list.add_host(packet[IP].src)
+        host_list.add_host(packet[IP].dst)
+        host_list.add_udp_port(packet[IP].src, packet[UDP].sport, Host.PORT_SRC)
+        host_list.add_udp_port(packet[IP].dst, packet[UDP].dport, Host.PORT_DST)
+
+    num_packets_parsed += 1
+
 
 
 class Host:
 
+    STATUS_UNK = 0
+    STATUS_UP = 1
 
-    def __init__(self, ip_addr='', no_dns_resolve=False):
+    STATUS_STRINGS = ["UNKNOWN", "UP"]
+
+    PORT_SRC = 0
+    PORT_DST = 1
+
+
+
+    def __init__(self, ip_addr=''):
         self.ip_addr = ip_addr
-        self.tcp_port_list = []
-        self.udp_port_list = []
-        self.no_dns_resolve = no_dns_resolve
-        self.hostname = ""
-        try:
-            if not self.no_dns_resolve:
-                self.hostname = socket.gethostbyaddr(self.ip_addr)[0]
-        except socket.herror:
-            self.hostname = "Unknown"
+        self.tcp_port_set = set()
+        self.udp_port_set = set()
+        self.hostname = "Unknown"
+        self.status = Host.STATUS_UNK
+
+    def resolve_name(self):
+        #try:
+        #    self.hostname = socket.gethostbyaddr(self.ip_addr)[0]
+        #except socket.herror:
+        #    self.hostname = "Unknown"
+        self.d = client.lookupPointer(name=self.reverse_name_for_ip_address())
+        self.d.addCallback(self.lookup_ptr_callback)
+        return self.d
+
+
+#        if answers:
+#            self.hostname = answers[0]
+
+    def lookup_ptr_callback(self, result):
+        answers, authority, additional = result
+        if answers:
+            return answers
+
+    def dns_success(self, result):
+        self.hostname = result[0]
+        for res in result:
+            print(res)
+
+    def dns_error(selfs, failure):
+        import sys
+        sys.stderr.write(str(failure))
+
+    def reverse_name_for_ip_address(self):
+        return '.'.join(reversed(self.ip_addr.split('.'))) + '.in-addr.arpa'
 
     def __lt__(self, other):
         return self.ip_addr < other.ip_addr
 
-    def add_tcp_port(self, tcp_port):
-        if tcp_port not in self.tcp_port_list:
-            bisect.insort(self.tcp_port_list, tcp_port)
+    def __eq__(self, other):
+        return self.ip_addr == other.ip_addr
+
+    def __gt__(self, other):
+        return self.ip_addr > other.ip_addr
+
+    def __str__(self):
+        return self.ip_addr
+
+    def __hash__(self):
+        return hash(self.ip_addr)
+
+    def add_tcp_port(self, tcp_port, port_direction):
+        if tcp_port not in self.tcp_port_set:
+            self.tcp_port_set.add(tcp_port)
+        if port_direction == Host.PORT_SRC:
+            self.status = Host.STATUS_UP
 
 
-    def add_udp_port(self, udp_port):
-        if udp_port not in self.udp_port_list:
-            bisect.insort(self.udp_port_list, udp_port)
+    def add_udp_port(self, udp_port, port_direction):
+        if udp_port not in self.udp_port_set:
+            self.udp_port_set.add(udp_port)
+        if port_direction == Host.PORT_SRC:
+            self.status = Host.STATUS_UP
 
-    def print(self):
-        if not self.no_dns_resolve:
-            print("Hostname: %s    " % self.hostname, end='')
-        print("Ip Addr: %s" % self.ip_addr)
-        for tport in self.tcp_port_list:
+    def print(self, include_hostnames=False):
+        print("Ip Addr: %s" % self.ip_addr, end='')
+        if include_hostnames == True:
+            print("    Hostname: %s" % self.hostname, end='')
+        print("    Status: %s" % Host.STATUS_STRINGS[self.status])
+        for tport in sorted(self.tcp_port_set):
             print ("    %i/tcp" % tport)
-        for uport in self.udp_port_list:
+        for uport in sorted(self.udp_port_set):
             print ("    %i/udp" % uport)
 
 
-class HostList:
 
-    def __init__(self, host_list=[], no_dns_resolve=False):
-        self.host_list = host_list
-        self.num = 0
-        self.no_dns_resolve = no_dns_resolve
+class HostList():
+
+    def __init__(self, host_set=set(), dns_resolve=False):
+        self.host_set = host_set
+        self.dns_resolve = dns_resolve
+
 
     def add_host(self, ip_addr):
-        host_addrs = []
-        if self.num != 0:
-            host_addrs = set(host.ip_addr for host in self.host_list)
-        if ip_addr not in host_addrs or self.num == 0:
-            host = Host(ip_addr, self.no_dns_resolve)
-            bisect.insort(self.host_list, host)
-            self.num += 1
+        global num_packets_parsed
+        host = Host(ip_addr)
+        if host not in self.host_set:
+            print("\r", end='')
+            print("[+][%i] New Host Found!  IP Addr: %s  " %(num_packets_parsed, ip_addr), end='')
+            #thread = threading.Thread(target = host.resolve_name())
+            #thread.start()
+            d = host.resolve_name()
+            d.addCallback(host.dns_success)
+            d.addErrback(host.dns_error)
+            self.host_set.add(host)
 
-    def add_tcp_port(self, ip_addr, port):
-        for host in self.host_list:
-            if host.ip_addr == ip_addr:
-                host.add_tcp_port(port)
-                break
+    def add_tcp_port(self, ip_addr, port, port_direction):
+        temp_host = Host(ip_addr)
+        for host in self.host_set:
+            if host == temp_host:
+                host.add_tcp_port(port, port_direction)
 
-    def add_udp_port(self, ip_addr, port):
-        for host in self.host_list:
-            if host.ip_addr == ip_addr:
-                host.add_udp_port(port)
-                break
+    def add_udp_port(self, ip_addr, port, port_direction):
+        temp_host = Host(ip_addr)
+        for host in self.host_set:
+            if host == temp_host:
+                host.add_udp_port(port, port_direction)
 
     def print(self):
-        for host in self.host_list:
-            host.print()
+        for host in sorted(self.host_set):
+            host.print(self.dns_resolve)
 
 
 parser = argparse.ArgumentParser(description='Parse pcap file for hosts, protocols, and ports')
@@ -100,9 +163,15 @@ parser.add_argument('-n', '--no-dns-resolve', required=False, action='store_true
 args = parser.parse_args()
 
 pcap_file = args.pcap_file
-
-host_list = HostList([], args.no_dns_resolve)
+host_list = HostList(set(), not args.no_dns_resolve)
 
 packets = sniff(offline=pcap_file, prn=parse_packet, store=0)
 
+reactor.callLater(60, reactor.stop); reactor.run()
+
 host_list.print()
+end = time.time()
+hours, rem = divmod(end-start, 3600)
+minutes, seconds = divmod(rem, 60)
+print("\nElapsed Execution Time: {:0>2}:{:0>2}:{:05.2f}".format(int(hours),int(minutes),seconds))
+
