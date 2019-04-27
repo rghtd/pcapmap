f4d8fb8dd8289c6eff5ce1fd045f0c3e0113fef9
rghtd <4896526+rghtd@users.noreply.github.com>
2019-04-27 12:18:00 -0400

    Consolidated into one file

--- pcapmap.py
+++ pcapmap.py
@@ -3,8 +3,7 @@
 from scapy.layers.inet import IP, TCP, UDP
 from scapy.sendrecv import sniff
 
-import argparse
-from HostList import HostList
+import argparse, bisect, socket
 
 ip_src = ''
 ip_dest = ''
@@ -25,6 +24,74 @@ def parse_packet(packet):
             host_list.add_udp_port(packet[IP].dst, packet[UDP].dport)
 
 
+class Host:
+
+
+    def __init__(self, ip_addr='', no_dns_resolve=False):
+        self.ip_addr = ip_addr
+        self.tcp_port_list = []
+        self.udp_port_list = []
+        self.no_dns_resolve = no_dns_resolve
+        self.hostname = ""
+        try:
+            if not self.no_dns_resolve:
+                self.hostname = socket.gethostbyaddr(self.ip_addr)[0]
+        except socket.herror:
+            self.hostname = "Unknown"
+
+    def __lt__(self, other):
+        return self.ip_addr < other.ip_addr
+
+    def add_tcp_port(self, tcp_port):
+        if tcp_port not in self.tcp_port_list:
+            bisect.insort(self.tcp_port_list, tcp_port)
+
+
+    def add_udp_port(self, udp_port):
+        if udp_port not in self.udp_port_list:
+            bisect.insort(self.udp_port_list, udp_port)
+
+    def print(self):
+        if not self.no_dns_resolve:
+            print("Hostname: %s    " % self.hostname, end='')
+        print("Ip Addr: %s" % self.ip_addr)
+        for tport in self.tcp_port_list:
+            print ("    %i/tcp" % tport)
+        for uport in self.udp_port_list:
+            print ("    %i/udp" % uport)
+
+
+class HostList:
+
+    def __init__(self, host_list=[], no_dns_resolve=False):
+        self.host_list = host_list
+        self.num = 0
+        self.no_dns_resolve = no_dns_resolve
+
+    def add_host(self, ip_addr):
+        host_addrs = []
+        if self.num != 0:
+            host_addrs = set(host.ip_addr for host in self.host_list)
+        if ip_addr not in host_addrs or self.num == 0:
+            host = Host(ip_addr, self.no_dns_resolve)
+            bisect.insort(self.host_list, host)
+            self.num += 1
+
+    def add_tcp_port(self, ip_addr, port):
+        for host in self.host_list:
+            if host.ip_addr == ip_addr:
+                host.add_tcp_port(port)
+                break
+
+    def add_udp_port(self, ip_addr, port):
+        for host in self.host_list:
+            if host.ip_addr == ip_addr:
+                host.add_udp_port(port)
+                break
+
+    def print(self):
+        for host in self.host_list:
+            host.print()
 
 
 parser = argparse.ArgumentParser(description='Parse pcap file for hosts, protocols, and ports')
@@ -37,11 +104,5 @@ pcap_file = args.pcap_file
 host_list = HostList([], args.no_dns_resolve)
 
 packets = sniff(offline=pcap_file, prn=parse_packet, store=0)
-#packets = rdpcap(pcap_file)
-#for packet in packets:
-#    parse_packet(packet)
 
 host_list.print()
-
-
-
